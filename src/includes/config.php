42b529a112a49757241d1682360728d7d82d3c51
SophimPhath <sophim@tarantulaconsulting.com>
2025-03-20 17:03:41 +0700

    refactor: update config

--- src/includes/config.php
+++ src/includes/config.php
@@ -25,7 +25,7 @@ if (getenv('APP_ENV') === 'production') {
 define('DB_SERVER', $_ENV['DB_SERVER'] ?? '127.0.0.1');
 define('DB_USER', $_ENV['DB_USER'] ?? 'root');
 define('DB_PASS', $_ENV['DB_PASS'] ?? '');
-define('DB_NAME', $_ENV['DB_NAME'] ?? 'hms_db');
+define('DB_NAME', $_ENV['DB_NAME'] ?? 'cms_db');
 
 // Connect to MySQL
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
